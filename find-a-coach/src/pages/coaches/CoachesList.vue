65814f7cb525e547d4c584b5c0999ea0ca85eb3b
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-10-14 13:39:23 +0200

    feat: Created Base components and connected them to existing components

--- find-a-coach/src/pages/coaches/CoachesList.vue
+++ find-a-coach/src/pages/coaches/CoachesList.vue
@@ -1,27 +1,30 @@
 <template>
   <section>FILTER</section>
   <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
-    </div>
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :areas="coach.areas"
-        :hourly-rate="coach.hourlyRate"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found</h3>
+    <base-card>
+      <div class="controls">
+        <base-button mode="outline">Refresh</base-button>
+        <base-button link to="register"> Register as a Coach </base-button>
+      </div>
+      <ul v-if="hasCoaches">
+        <coach-item
+          v-for="coach in filteredCoaches"
+          :key="coach.id"
+          :id="coach.id"
+          :first-name="coach.firstName"
+          :last-name="coach.lastName"
+          :areas="coach.areas"
+          :hourly-rate="coach.hourlyRate"
+        ></coach-item>
+      </ul>
+      <h3 v-else>No coaches found</h3>
+    </base-card>
   </section>
 </template>
 
 <script>
 import CoachItem from "@/components/coaches/CoachItem.vue";
+import { useLink } from "vue-router";
 
 export default {
   components: {
