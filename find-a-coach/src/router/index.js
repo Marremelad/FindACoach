521d545eec1174f2a88c85284cbef66fca30d724
Mauricio Corte <Corte.Mauricio98@gmail.com>
2025-10-14 08:07:21 +0200

    (feature) Added routing

--- find-a-coach/src/router/index.js
+++ find-a-coach/src/router/index.js
@@ -1,8 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      redirect: "/coaches",
+    },
+    {
+      path: "/coaches",
+      component: null,
+    },
+    {
+      path: "/coaches/:id",
+      component: null,
+      children: [
+        {
+          path: "contact", // /coaches/c1/contact
+          component: null,
+        },
+      ],
+    },
+    {
+      path: "/register",
+      component: null,
+    },
+    {
+      path: "/requests",
+      component: null,
+    },
+    {
+      path: "/:catchAll(.*)",
+      component: null,
+    },
+  ],
+});
 
-export default router
+export default router;
